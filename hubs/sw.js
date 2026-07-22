40c7331dc37861fe5152d9c5fa7da301beaad38c
centiger <centiger2@gmail.com>
2026-07-22 20:09:21 +0900

    Add files via upload

--- hubs/sw.js
+++ hubs/sw.js
@@ -1,2 +1,12 @@
-const CACHE='cen-divided-hubs-review-20260610';self.addEventListener('install',e=>self.skipWaiting());self.addEventListener('activate',e=>self.clients.claim());
-/* 20260616-next-era-links */
+const CACHE='cen-divided-hubs-bible-link-20260722';
+const ASSETS=['./','./index.html','./manifest.json','./css/style.css','./js/app.js','./data/hubs.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request)));
+});
